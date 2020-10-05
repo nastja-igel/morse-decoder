d9b607151f2ef6422edfa4286303ee4a474c6b30
Ilina <anastasia.ilina@t-systems.ru>
2020-10-05 22:49:18 +0300

    feat: quick solution for the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,18 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const array = [];
+
+    for (let i = 0; i < expr.length - 1; i += 10) {
+        array.push(MORSE_TABLE [str
+                .slice(i, i + 10)
+                .replace(/00/g, "")
+                .replace(/11/g, "-")
+                .replace(/10/g, ".")
+                .replace(/\*{10}/, " ")]);
+    }
+
+    return array.join ``;
 }
 
 module.exports = {
